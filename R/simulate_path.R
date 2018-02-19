1fde0aa7eff4f34b18787e1e271628f512f9a779
Iegor Rudnytskyi <yegor.rudnytskiy@gmail.com>
2018-02-19 14:07:00 +0100

    Add names of arguments

--- R/simulate_path.R
+++ R/simulate_path.R
@@ -1,20 +1,35 @@
 # max_jumps_number can be Inf
-simulate_process <- function(u, premiums, lambda1, lambda2,
+simulate_process <- function(initial_capital = 10,
+                             premium_rate = 1,
+                             capital_injections_rate = 1,
+                             capital_injections_distribution = rexp,
+                             capital_injections_distribution_parameters = list(rate = 1),
+                             claims_rate = 1,
+                             claims_ht_distribution = rexp,
+                             claims_ht_distribution_parameters = list(rate = 1),
+                             claims_pt_distribution = rpareto,
+                             claims_pt_distribution_parameters = list(shape = 1, scale = 1),
+                             mixing_parameter = 0.5,
                              max_jumps_number = 10000) {
+    # rename arguments
+    u <- initial_capital
+    pr <- premium_rate
+    l1 <- capital_injections_rate
+    l2 <- claims_rate
 
-
-    # browser()
-
+    # initialize process
     process <- matrix(NA, nrow = 1, ncol = 2)
     colnames(process) <- c("time", "X")
     process[1, ] <- c(0, u)
 
+
+    # adding negative jump to a process
     add_njump <- function(jump_time, jump_value) {
         time_to_jump <- jump_time - process[nrow(process), 1]
         process <<- rbind(
             process,
             c(jump_time,
-              process[nrow(process), 2] + time_to_jump * premiums)
+              process[nrow(process), 2] + time_to_jump * pr)
         )
 
         process <<- rbind(
@@ -26,12 +41,13 @@ simulate_process <- function(u, premiums, lambda1, lambda2,
         jumps_number <<- jumps_number + 1
     }
 
+    # adding positive jump to a process
     add_pjump <- function(jump_time, jump_value) {
         time_to_jump <- jump_time - process[nrow(process), 1]
         process <<- rbind(
             process,
             c(jump_time,
-              process[nrow(process), 2] + time_to_jump * premiums)
+              process[nrow(process), 2] + time_to_jump * pr)
         )
 
         process <<- rbind(
@@ -43,15 +59,17 @@ simulate_process <- function(u, premiums, lambda1, lambda2,
         jumps_number <<- jumps_number + 1
     }
 
+    # get last value of a process
     get_process_last <- function() process[nrow(process), 2]
 
+    # get last element of a process
     last <- function(x) x[length(x)]
 
     p_time <- numeric() # time of positive jumps
     n_time <- numeric() # time of negative jumps
 
-    p_time[1] <-  rexp(1, lambda1)
-    n_time[1] <-  rexp(1, lambda2)
+    p_time[1] <-  rexp(1, l1)
+    n_time[1] <-  rexp(1, l2)
 
 
     jumps_number <- 0
@@ -70,7 +88,7 @@ simulate_process <- function(u, premiums, lambda1, lambda2,
             }
 
             repeat {
-                n_time <-  c(n_time, last(n_time) + rexp(1, lambda2))
+                n_time <-  c(n_time, last(n_time) + rexp(1, l2))
                 if(last(p_time) < last(n_time)) break
                 add_njump(last(n_time), 1)
                 if(get_process_last() < 0) return(process)
@@ -93,7 +111,7 @@ simulate_process <- function(u, premiums, lambda1, lambda2,
             }
 
             repeat {
-                p_time <-  c(p_time, last(p_time) + rexp(1, lambda1))
+                p_time <-  c(p_time, last(p_time) + rexp(1, l1))
                 if(last(p_time) > last(n_time)) break
                 add_pjump(last(p_time), 1)
                 if(jumps_number > max_jumps_number) {
@@ -106,153 +124,5 @@ simulate_process <- function(u, premiums, lambda1, lambda2,
 
         }
     }
-    return(process)
+    # return(process)
 }
-
-set.seed(1)
-prc <- simulate_process(u = 10, premiums = 1, lambda1 = 1, lambda2 = 1)
-prc <- simulate_process(u = 2, premiums = 1, lambda1 = 1, lambda2 = 1)
-
-
-# tests
-plot(sapply(1:length(n_time), function(x) mean(diff(n_time[1:x]))), type = "l")
-length(n_time) - length(p_time)
-
-
-set.seed(1)
-prc <- simulate_process(u = 7, premiums = 1, lambda1 = 1, lambda2 = 2)
-plot(prc, type = "l")
-plot(prc[1:100, ], type = "l")
-
-
-# fast matrix append without creating new matrix
-# a <- matrix(1:4, ncol = 2, byrow = TRUE)
-# a <- t(a)
-# a[c(5, 6)] <- c(5, 6)
-# matrix(a, ncol = 2, byrow = TRUE)
-
-# how break works
-# i <- 1
-# while(i < 10) {
-#     j <- 1
-#     repeat {
-#         j <- j + 1
-#         if(j > 7) break
-#     }
-#     i <- i + 1
-# }
-
-
-
-
-# add_njump <- function(jump_time, jump_value) {
-#     process[nrow(process) + 1, 1] <- process[nrow(process), 1] + jump_time
-#     process[nrow(process), 2] <- process[nrow(process) - 1, 2] + jump_time * premiums
-#     process[nrow(process) + 1, 1] <- process[nrow(process), 1]
-#     process[nrow(process), 2] <- process[nrow(process) - 1, 2] - jump_time
-# }
-#
-# add_pjump <- function(jump_time, jump_value) {
-#     process[nrow(process) + 1, 1] <- process[nrow(process), 1] + jump_time
-#     process[nrow(process), 2] <- process[nrow(process) - 1, 2] + jump_time * premiums
-#     process[nrow(process) + 1, 1] <- process[nrow(process), 1]
-#     process[nrow(process), 2] <- process[nrow(process) - 1, 2] + jump_time
-#
-# }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# simulate_process_until_ruin <- function(u, premiums, lambda1, lambda2,
-#                                         max_iterations, max_time) {
-#
-#     browser()
-#
-#     process <- matrix(NA, nrow = 1, ncol = 2)
-#     colnames(process) <- c("time", "X")
-#     process[1, ] <- c(0, u)
-#
-#     add_njump <- function(jump_time, jump_value) {
-#         time_to_jump <- jump_time - process[nrow(process), 1]
-#         process <<- rbind(
-#             process,
-#             c(process[nrow(process), 1] + jump_time,
-#               process[nrow(process), 2] + time_to_jump * premiums)
-#         )
-#
-#         process <<- rbind(
-#             process,
-#             c(process[nrow(process), 1],
-#               process[nrow(process), 2] - jump_value)
-#         )
-#     }
-#
-#     add_pjump <- function(jump_time, jump_value) {
-#         time_to_jump <- jump_time - process[nrow(process), 1]
-#         process <<- rbind(
-#             process,
-#             c(process[nrow(process), 1] + jump_time,
-#               process[nrow(process), 2] + time_to_jump * premiums)
-#         )
-#
-#         process <<- rbind(
-#             process,
-#             c(process[nrow(process), 1],
-#               process[nrow(process), 2] + jump_value)
-#         )
-#     }
-#
-#     get_process_last <- function() process[nrow(process), 2]
-#
-#     last <- function(x) x[length(x)]
-#
-#     p_time <- numeric(); p_time[1] <- 0 # time of positive jumps
-#     n_time <- numeric(); n_time[1] <- 0 # time of negative jumps
-#
-#     # iterator
-#     # i <- 0
-#
-#     p_time <-  c(p_time, last(p_time) + rexp(1, lambda1))
-#     n_time <-  c(n_time, last(n_time) + rexp(1, lambda2))
-#
-#     repeat{
-#
-#         if(last(p_time) > last(n_time)) {
-#
-#             add_njump(last(n_time), 1)
-#             if(get_process_last() < 0) break
-#
-#             while(last(p_time) > last(n_time)) {
-#                 n_time <-  c(n_time, last(n_time) + rexp(1, lambda2))
-#                 add_njump(last(n_time), 1)
-#                 if(get_process_last() < 0) break
-#             }
-#
-#             add_pjump(last(p_time), 1)
-#
-#         } else {
-#
-#             add_pjump(last(p_time), 1)
-#
-#             while(last(p_time) < last(n_time)) {
-#                 p_time <-  c(p_time, last(p_time) + rexp(1, lambda1))
-#                 add_pjump(last(p_time), 1)
-#             }
-#
-#             add_njump(last(n_time), 1)
-#             if(get_process_last() < 0) break
-#         }
-#     }
-#     return(process)
-# }
