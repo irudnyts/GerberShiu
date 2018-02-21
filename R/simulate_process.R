b2a786614efdc7d72006c277803d716da92ece5f
Iegor Rudnytskyi <yegor.rudnytskiy@gmail.com>
2018-02-21 16:34:50 +0100

    Add documetnation of simulate_process function. Add number of jumps to a `process` class.

--- R/simulate_process.R
+++ R/simulate_process.R
@@ -1,5 +1,65 @@
 # max_jumps_number can be Inf
 # set seed
+
+#' Simulate a path of Cramer–Lundberg model extenssion with capital injections
+#'
+#' \code{simulate_process} returns an object of S3 \code{process} class, which
+#' is simulated stochastic process of Cramer–Lundberg model extenssion with
+#' capital injections (i.e. positive jumps).
+#'
+#' This function simulates a stochastic process, which follows Cramer–Lundberg
+#' model extenssion with capital injections (i.e. positive jumps) until either
+#' ruin or when maximum number of jumps is attained.
+#'
+#' @section Warning:
+#' The function should be used with a great care when \code{max_jumps_number} is
+#' set to \code{Inf}. In this case a simulating \code{repeat} loop will be
+#' stopped only by ruin. This can cause an infinite loop.
+#'
+#' @param u a numeric vector of length one specifying an initial capital
+#' (\eqn{u} in the formula representation). Default value is 10.
+#' @param pr a numeric vector of length one specifying a premium rate (\eqn{c}
+#' in the formula representation). Defaulr value is 1.
+#' @param lambda_p a numeric vector of length one specifying the rate of a
+#' Poisson process of capital injections. Default value is 1.
+#' @param f_p a function specifying a random generator for a distribution of
+#' capital injections. Default value is \code{rexp} (an exponential
+#' distribution).
+#' @param param_p a list of parameters of capital injections' distribution
+#' passed to f_p. The default value is \code{list(rate = 1)}.
+#' @param lambda_n a numeric vector of length one specifying the rate of a
+#' Poisson process of claims. Default value is 1.
+#' @param f_n1 a function specifying a random generator for a phase-type
+#' distribution of claims. Default value is \code{rexp} (an exponential
+#' distribution).
+#' @param param_n1 a list of parameters of claims' distribution passed to f_n1.
+#' The default value is \code{list(rate = 1 / 2)}.
+#' @param f_n2 a function specifying a random generator for a heavy-tailed
+#' distribution of claims. Default value is \code{actuar::rpareto1} (a Pareto
+#' Type I distribution, from \code{actuar} package).
+#' @param param_n1 a list of parameters of claims' distribution passed to f_n2.
+#' The default value is \code{list(shape = 3 / 2, min = 2 / 3)}.
+#' @param eps a numeric vector of length one specifying the mixing parameter for
+#' claims' mixed distribution.
+#' @param max_jumps_number a numeric vector of length one specifying the maximum
+#' number of positive and negative jumps, after which the function stops.
+#' Default value is 10000. Can be set to \code{Inf}.
+#' @return An object of class \code{process} which is represented by a list of
+#' elements:
+#' \itemize{
+#' \item path: a matrix of two columns. The first column is the time, the second
+#' is the value of a process.
+#' \item jumps_p a vector of positive (capital injections) jumps' values.
+#' \item time_p a vector indicating times of positive jumps.
+#' \item jumps_n a vector of negative (claims) jumps' values.
+#' \item time_p a vector indicating times of negative jumps.
+#' \item jumps_number a vector indicating the number of all jumps.
+#' }
+#'
+#' @examples
+#' \dontrun{
+#' prc <- simulate_process()
+#' }
 #' @export
 simulate_process <- function(u = 10,
                              pr = 1,
@@ -12,7 +72,7 @@ simulate_process <- function(u = 10,
                              f_n2 = actuar::rpareto1,
                              param_n2 = list(shape = 3 / 2, min = 2 / 3),
                              eps = 0.1,
-                             max_jumps_number = 1000000) {
+                             max_jumps_number = 10000) {
 
     # add n = 1 to all distributions parameters in order to generate only
     # one r.v.
@@ -89,6 +149,8 @@ simulate_process <- function(u = 10,
     time_n[1] <- rexp(1, lambda_n)
     time_p[1] <- rexp(1, lambda_p)
 
+    # browser()
+
     repeat{
 
         if(last(time_p) > last(time_n)) {
@@ -140,7 +202,8 @@ simulate_process <- function(u = 10,
         jumps_p = jumps_p,
         time_p = time_p,
         jumps_n = jumps_n,
-        time_n = time_n
+        time_n = time_n,
+        jumps_number = jumps_number
     )
 
     class(rval) <- "process"
