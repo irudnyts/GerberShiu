1c0c52d3616a5695a3b442fd4b2740845f9f8a66
Iegor Rudnytskyi <yegor.rudnytskiy@gmail.com>
2018-02-21 18:39:40 +0100

    Update tests to start from different seeds.

--- tests/testthat/test_class_consistency.R
+++ tests/testthat/test_class_consistency.R
@@ -1,23 +1,28 @@
 context("Class' elements consistency")
 
-set.seed(1000)
-process <- simulate_process()
+for(seed in 1:100) {
+    set.seed(seed)
+    process <- simulate_process(max_jumps_number = 100)
 
-test_that(
-    paste0("Number of positive jumps is consistent with the number of",
-                 " positive jumps' arrivals time"),
-    expect_equal(length(process$jumps_p), length(process$time_p))
-)
+    test_that(
+        paste0("Seed: ", seed,
+               ". Number of positive jumps is consistent with the number of",
+               " positive jumps' arrivals time"),
+        expect_equal(length(process$jumps_p), length(process$time_p))
+    )
 
-test_that(
-    paste0("Number of negative jumps is consistent with the number of",
-           " negative jumps' arrivals time"),
-    expect_equal(length(process$jumps_n), length(process$time_n))
-)
+    test_that(
+        paste0("Seed: ", seed,
+               ". Number of negative jumps is consistent with the number of",
+               " negative jumps' arrivals time"),
+        expect_equal(length(process$jumps_n), length(process$time_n))
+    )
 
-test_that(
-    paste0("Number of negative jumps is consistent with the number of",
-           " negative jumps' arrivals time"),
-    expect_equal(process$jumps_number,
-                 length(process$jumps_p) + length(process$jumps_n))
-)
+    test_that(
+        paste0("Seed: ", seed,
+               ". Number of negative jumps is consistent with the number of",
+               " negative jumps' arrivals time"),
+        expect_equal(process$jumps_number,
+                     length(process$jumps_p) + length(process$jumps_n))
+    )
+}
--- tests/testthat/test_jumps.R
+++ tests/testthat/test_jumps.R
@@ -1,12 +1,17 @@
 context("Jumps' interarrival times")
 
-set.seed(1000)
-process <- simulate_process()
 
-test_that("Mean of positive jumps' is around 1", {
-    expect_true(abs(mean(process$jumps_p) - 1) < 0.1)
-})
+for(seed in 1:100) {
+    set.seed(seed)
+    process <- simulate_process(u = 100, max_jumps_number = 1000)
 
-test_that("Mean of negative jumps' is around 2", {
-    expect_true(abs(mean(process$jumps_n) - 2) < 0.1)
-})
+    test_that(
+        paste0("Seed: ", seed, ". Mean of positive jumps' is around 1"),
+        expect_true(abs(mean(process$jumps_p) - 1) < 0.5)
+    )
+
+    test_that(
+        paste0("Seed: ", seed, ". Mean of negative jumps' is around 2"),
+        expect_true(abs(mean(process$jumps_n) - 2) < 0.5)
+    )
+}
