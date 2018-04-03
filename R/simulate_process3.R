96d872c797b3e3042493d48e2e74039c292dac38
Iegor Rudnytskyi <yegor.rudnytskiy@gmail.com>
2018-04-03 14:32:03 +0200

    Change algorythm 1, minor changes for algorithm 3

--- R/simulate_process3.R
+++ R/simulate_process3.R
@@ -10,7 +10,8 @@ simulate_process3 <- function(u = 10,
                               param_n2 = list(shape = 3 / 2, min = 2 / 3),
                               eps = 0.1,
                               max_jumps_number = 10000,
-                              max_time_span = 7500) {
+                              max_time = 7500) {
+    #browser()
     # add n = 1 to all distributions parameters in order to generate only
     # one r.v.
     param_p[["n"]] <- 1
@@ -44,8 +45,8 @@ simulate_process3 <- function(u = 10,
     a_pos <- numeric() # arrival times of positive jumps
     a_neg <- numeric() # arrival times of negative jumps
 
-    ca_pos <- rexp(1, lambda_p) # current arrival of a positive jump
-    ca_neg <- rexp(1, lambda_n) # current arrival of a negative jump
+    ca_pos <- rexp(1, lambda_p) # current arrival time of a positive jump
+    ca_neg <- rexp(1, lambda_n) # current arrival time of a negative jump
 
     repeat{
 
@@ -54,7 +55,10 @@ simulate_process3 <- function(u = 10,
 
             if(ca_pos > ca_neg) {
 
-                cs_neg <- do.call(f_n, param_n) # current negative jump's size
+                # current negative jump's size
+                cs_neg <- ifelse(test = rbinom(n = 1, size = 1, prob = eps) == 1,
+                                 yes = do.call(f_n2, param_n2),
+                                 no = do.call(f_n1, param_n1))
 
                 path <- add_jump_to_path(path, ca_neg, -cs_neg)
 
@@ -69,7 +73,10 @@ simulate_process3 <- function(u = 10,
             } else if(ca_pos == ca_neg) {
 
                 cs_pos <- do.call(f_p, param_p) # current positive jump's size
-                cs_neg <- do.call(f_n, param_n) # current negative jump's size
+                # current negative jump's size
+                cs_neg <- ifelse(test = rbinom(n = 1, size = 1, prob = eps) == 1,
+                                 yes = do.call(f_n2, param_n2),
+                                 no = do.call(f_n1, param_n1))
 
                 path <- add_jump_to_path(path, ca_pos, cs_pos - cs_neg)
 
@@ -78,6 +85,8 @@ simulate_process3 <- function(u = 10,
 
                 a_pos <- c(a_pos, ca_pos)
                 a_neg <- c(a_neg, ca_neg)
+
+                if(path[nrow(path), 2] < 0) break
 
                 ca_pos <- ca_pos + rexp(1, lambda_p)
                 ca_neg <- ca_neg + rexp(1, lambda_n)
@@ -102,4 +111,29 @@ simulate_process3 <- function(u = 10,
 
     }
 
+    if(path[nrow(path), 2] >= 0)
+        path <- rbind(
+            path,
+            c(max_time,
+              path[nrow(path), 2] + (max_time - path[nrow(path), 1]) * pr)
+        )
+
+
+    rval <- list(path = path,
+                 positive_jump_size = s_pos,
+                 negative_jump_size = s_neg,
+                 positive_arrival_time = a_pos,
+                 negative_arrival_time = a_neg)
+
+    class(rval) <- "process"
+
+    return(rval)
+
 }
+
+
+# simulate_process3(u = 10, pr = 0.1,
+#                  lambda_p = 1, f_p = rexp, param_p = list(rate = 1),
+#                  lambda_n = 1, f_n1 = rexp, param_n1 = list(rate = 1),
+#                  eps = 0,
+#                  max_time = 10)
